e5e2dd69da64b9791c21a473c4641a263245f189
jhonjpg <jonathan050315jj@gmail.com>
2025-06-08 13:20:34 -0400

    first done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    base: '/boquillaResponde/', // <-- SLASH al inicio y al final
     plugins: [tailwindcss()]
 
 })
